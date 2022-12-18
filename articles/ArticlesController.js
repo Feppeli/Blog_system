5b91cca1c5343366e61e626a4b48009d11bf0870
Your Name <dfsilvat@gmail.com>
2022-12-18 10:32:54 -0300

    cad articles

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,12 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const Categorie = require('../categories/Categorie')
+const Article = require('./Article')
+const Slugify = require('slugify');
+const { default: slugify } = require('slugify');
 
-router.get('/articles', (req, res) => {
-    res.send('ROTA DE ARTIGOS')
+router.get('/admin/articles', (req, res) => {
+    Article.findAll()
+    .then(articles => {
+        res.render('admin/articles/index',{
+            articles: articles
+        })
+    })
 });
 
 router.get('/admin/articles/new', (req, res) =>{
-    res.render('admin/articles/new')
+    Categorie.findAll()
+    .then(categories => {
+        res.render('admin/articles/new', {
+            categories: categories
+        })
+    })
+
+})
+
+router.post('/articles/save', (req, res) => {
+    var title = req.body.title;
+    var body = req.body.body;
+    var categorie = req.body.categorie
+
+    Article.create({
+        title: title,
+        body: body,
+        slug: slugify(title),
+        categoryId : categorie
+    })
+    .then(()=> {
+        res.redirect('/admin/articles')
+    })
 })
 
 module.exports = router
--- database/db.js
+++ database/db.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize');
 
 const connection = new Sequelize('Blog', 'root', 'root', {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: '-03:00' /* timezone do Brasil */
 });
 
 module.exports = connection;
